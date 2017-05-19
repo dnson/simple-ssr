4bef53228bb56cb8feacf120cd2ecf564e01d2a6
dnson <dnson272@gmail.com>
2017-05-20 06:35:33 +0700

    SSR with fetch

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
 import React from 'react'
-const App = () => <div>Hello React</div> 
+const App = ({ gists }) => (
+     <ul>
+       {gists.map(gist => (
+         <li key={gist.id}>{gist.url}</li>
+        ))} 
+    </ul>
+)
+App.propTypes = {
+     gists: React.PropTypes.array,
+}
 export default App
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import React from 'react'
+import fetch from 'isomorphic-fetch'
 import ReactDOM from 'react-dom/server'
 import App from './app'
 import template from './template'
@@ -7,10 +8,15 @@ import template from './template'
 const app = express()
 app.use(express.static('dist/public'))
 
-app.get('/', (req, res) => {
-     const body = ReactDOM.renderToString(<App />)
-     const html = template(body)
-     res.send(html)
+ app.get('/', (req, res) => {
+     fetch('https://api.github.com/users/dnson/gists')
+       .then(response => response.json())
+       .then(gists => {
+           console.log(gists);
+         const body = ReactDOM.renderToString(<App gists={gists} />)
+         const html = template(body, gists)
+         res.send(html)
+       })
 })
 app.listen(3000, () => {
      console.log('Listening on port 3000')
